e103c8bf29c15de9389c5b7c4197a7c428c63a64
Gustavo Medeiros <78024109+Gustavo067@users.noreply.github.com>
2026-05-21 16:17:00 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import fastify from 'fastify'
-
+import { knex } from './database'
 const app = fastify()
 
 app.get('/hello',async  () => {
-  
+  const transaction = await knex.transaction("transaction")
+  .where("amount", 1000)
+  .select("*")
+
+  return transaction
 })
 
 app
   .listen({
-     port: 3333,
+     port: env.PORT,
      }).then(() => {
   console.log('Server is running on port 3333')
 })
